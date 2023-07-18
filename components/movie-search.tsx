1deeef21efee9d5f83f52d05e03a2ee3058b3e0d
Ozhan Efe Meral <6693ozzy@gmail.com>
2023-07-18 22:03:39 +0300

    Fix: Change query param naming
    -change "title" to "query"

--- components/movie-search.tsx
+++ components/movie-search.tsx
@@ -7,8 +7,8 @@ import { useState } from "react";
 export default function MovieSearch() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const title = searchParams.get("title");
-  const [searchInput, setSearchInput] = useState(title || "");
+  const query = searchParams.get("query");
+  const [searchInput, setSearchInput] = useState(query || "");
 
   return (
     <form action="/movies" method="GET">
@@ -17,10 +17,10 @@ export default function MovieSearch() {
         <Input type="hidden" name="results_per_page" value="12" />
         <Input
           type="text"
-          name="title"
+          name="query"
           minLength={2}
           placeholder="Start searching movies"
-          defaultValue={title || ""}
+          defaultValue={query || ""}
           onChange={(e) => setSearchInput(e.target.value)}
           className="flex-grow w-full md:w-auto"
         />
