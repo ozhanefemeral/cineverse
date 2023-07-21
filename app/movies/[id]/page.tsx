2c3b7d0ae3d683b3bcc47360c74d232a612bb1f0
Ozhan Efe Meral <6693ozzy@gmail.com>
2023-07-21 17:51:57 +0300

    Feature: Movie Cast & Recommendations
    -add cast row
    -add recommendations row
    -add recommendations to movie details

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,5 +1,6 @@
+import RecommendedMoviesRow from "@/components/recommendations/recommendations-row";
 import { moviedb } from "@/lib/tmdb";
-import { MovieResponse } from "moviedb-promise";
+import { CreditsResponse, MovieResponse } from "moviedb-promise";
 import Image from "next/image";
 
 export default async function MovieDetails({
@@ -8,9 +9,12 @@ export default async function MovieDetails({
   params: { id: string };
 }) {
   const details = (await moviedb.movieInfo({ id: params.id })) as MovieResponse;
+  const { cast } = (await moviedb.movieCredits({
+    id: params.id,
+  })) as CreditsResponse;
 
   return (
-    <div className="flex flex-col md:flex-row max-w-screen-lg justify-center">
+    <div className="flex flex-col flex-wrap md:flex-row justify-center">
       <div className="w-full md:w-1/3 p-4">
         <Image
           src={`https://image.tmdb.org/t/p/w500${details.poster_path}`}
@@ -24,6 +28,32 @@ export default async function MovieDetails({
         <h1 className="text-2xl font-bold">{details.title}</h1>
         <div className="text-primary">{details.tagline}</div>
         <div className="text-primary">{details.overview}</div>
+        {cast && (
+          <div className="py-4">
+            <h3 className="text-xl font-bold pb-2">Cast</h3>
+            <div className="flex flex-row flex-wrap justify-evenly gap-4">
+              {cast.slice(0, 5).map((actor) => (
+                <div
+                  key={actor.id}
+                  className="flex flex-col items-center border rounded-lg overflow-hidden"
+                >
+                  <Image
+                    src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
+                    alt={actor.name + " poster"}
+                    width={150}
+                    height={200}
+                  />
+                  <p className="text-center p-2 px-4">{actor.name}</p>
+                  <p className="text-center font-light text-gray-400">{actor.character}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="w-full p-4">
+        <h2 className="text-xl py-4 font-bold">You might also like these...</h2>
+        <RecommendedMoviesRow basedOn={params.id} />
       </div>
     </div>
   );
---
+++ components/recommendations/recommendations-row.tsx
@@ -0,0 +1,50 @@
+import { moviedb } from "@/lib/tmdb";
+import Image from "next/image";
+import Link from "next/link";
+
+async function getMovieRecommendations(movieId: string) {
+  const response = await moviedb.movieRecommendations({ id: movieId });
+
+  return response;
+}
+
+export default async function RecommendedMoviesRow({
+  basedOn,
+}: {
+  basedOn: string;
+}) {
+  const {
+    page,
+    results: movies,
+    total_pages: totalPages,
+    total_results: totalResults,
+  } = await getMovieRecommendations(basedOn);
+
+  if (totalResults === 0 || !movies)
+    return (
+      <p className="text-gray-400">No recommendations found 😿</p>
+    );
+
+  return (
+    <div className="flex flex-row gap-8 overflow-scroll w-full">
+      {movies.map((movie) => (
+        <Link
+          href={`/movies/${movie.id}`}
+          key={movie.id}
+          className="shrink-0 max-w-[200px]"
+        >
+          <div key={movie.id} className="flex flex-col gap-2">
+            <Image
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title + " poster"}
+              className="w-full rounded-lg shadow-md"
+              width={200}
+              height={300}
+            />
+            <p className="text-center text-sm">{movie.title}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
