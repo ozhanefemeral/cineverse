6580c11c8152964d3991953b1e271a5ec05def30
Ozhan Efe Meral <6693ozzy@gmail.com>
2023-07-22 14:36:42 +0300

    Fix: Navbar Navigation
    -fix page reload on navbar links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -18,44 +18,6 @@ import Image from "next/image";
 import cineverseLogo from "@/public/cineverse.svg";
 import { usePathname } from "next/navigation";
 
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-];
-
 export default function Navbar() {
   const path = usePathname();
 
@@ -81,20 +43,19 @@ export default function Navbar() {
           <NavigationMenuContent>
             <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
               <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="flex h-full w-full select-none flex-col rounded-md bg-gradient-to-b justify-end from-red-500 to-red-900 p-6 no-underline outline-none focus:shadow-md"
-                    href="/trending/movies"
-                  >
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      Trending Movies 🍿
-                    </div>
-                    <p className="text-sm leading-tight">
-                      Your Ticket to the Latest Hits: Unveiling the Trending
-                      Movies!
-                    </p>
-                  </a>
-                </NavigationMenuLink>
+                <Link href="/trending/movies" legacyBehavior passHref>
+                  <NavigationMenuLink asChild>
+                    <a className="flex h-full w-full select-none flex-col rounded-md bg-gradient-to-b justify-end from-red-500 to-red-900 p-6 no-underline outline-none focus:shadow-md">
+                      <div className="mb-2 mt-4 text-lg font-medium">
+                        Trending Movies 🍿
+                      </div>
+                      <p className="text-sm leading-tight">
+                        Your Ticket to the Latest Hits: Unveiling the Trending
+                        Movies!
+                      </p>
+                    </a>
+                  </NavigationMenuLink>
+                </Link>
               </li>
             </ul>
           </NavigationMenuContent>
@@ -104,11 +65,22 @@ export default function Navbar() {
           <NavigationMenuContent>
             <ul className="grid gap-3 p-4 md:w-[500px] grid-cols-3 md:grid-cols-4 lg:w-[600px] ">
               {movieCategories.map((category) => (
-                <ListItem
-                  key={category.id}
-                  title={category.name}
+                // <NavigationMenuLink asChild>
+                //   <ListItem
+                //     key={category.id}
+                //     title={category.name}
+                //     href={`/categories/${category.slug}`}
+                //   ></ListItem>
+                // </NavigationMenuLink>
+                <Link
                   href={`/categories/${category.slug}`}
-                ></ListItem>
+                  legacyBehavior
+                  passHref
+                >
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    {category.name}
+                  </NavigationMenuLink>
+                </Link>
               ))}
             </ul>
           </NavigationMenuContent>
