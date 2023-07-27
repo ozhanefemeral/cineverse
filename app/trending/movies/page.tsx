b3af3a34245de62eb7bf22f33bc3449ed879dfba
Ozhan Efe Meral <6693ozzy@gmail.com>
2023-07-27 16:38:50 +0300

    Refactor: Remove Trending Grid
    -remove trending grid component
    -add standard grid to trending page

--- app/trending/movies/page.tsx
+++ app/trending/movies/page.tsx
@@ -1,12 +1,36 @@
 import GridLoading from "@/components/grid-loading";
-import TrendingGrid from "@/components/trending/trending-grid";
+import Grid from "@/components/ui/grid";
+import MovieCard from "@/components/ui/movie/movie-card";
+import { moviedb } from "@/lib/tmdb";
+import { MovieResult } from "moviedb-promise";
 import { Suspense } from "react";
 
 export default async function TrendingMovies() {
+  const {
+    results: movies,
+    page,
+    total_pages: totalPages,
+    total_results: totalResults,
+  } = (await moviedb.trending({
+    media_type: "movie",
+    time_window: "week",
+  })) as {
+    results: MovieResult[];
+    page: number;
+    total_pages: number;
+    total_results: number;
+  };
+
+  if (!movies) return <div className="text-center">No movies found 😿</div>;
+
   return (
     <>
       <Suspense fallback={<GridLoading />}>
-        <TrendingGrid />
+        <Grid>
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </Grid>
       </Suspense>
     </>
   );
