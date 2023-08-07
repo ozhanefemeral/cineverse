1ca9be741e92ece22f5c442108b1517b96975e76
Ozhan Efe Meral <6693ozzy@gmail.com>
2023-08-07 17:37:09 +0300

    add Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/navbar";
 const inter = Inter({ subsets: ["latin"] });
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: "Cineverse",
@@ -17,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Analytics />
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark">
           <div className="lg:flex max-w-full">
